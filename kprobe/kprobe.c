2745afba83a65ceb8aa2f7139df1956d5278997e
linux <l3b2w1@gmail.com>
2017-06-02 16:47:03 +0800

    jprobe entry

--- kprobe/kprobe.c
+++ kprobe/kprobe.c
@@ -10,39 +10,78 @@
  * whenever do_fork() is invoked to create a new process.
  */
 
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/kernel.h>
 #include <linux/kprobes.h>
+#include <net/iw_handler.h>
+
+#define JPROBE_DEFINE(func)        \
+{                                  \
+    .entry = func##_entry,         \
+    .kp = {                        \
+        .symbol_name = #func   \
+    },                             \
+}
+
+static long do_fork_entry(unsigned long clone_flags, unsigned long stack_start,
+	      struct pt_regs *regs, unsigned long stack_size,
+	      int __user *parent_tidptr, int __user *child_tidptr)
+{
+	printk(KERN_INFO "jprobe: clone_flags = 0x%lx, stack_size = 0x%lx,"
+			" regs = 0x%p\n",
+	       clone_flags, stack_size, regs);
+
+	/* Always end with a call to jprobe_return(). */
+	jprobe_return();
+	return 0;
+}
+
+int ioctl_private_call_entry(struct net_device *dev, struct iwreq *iwr,
+        unsigned int cmd, struct iw_request_info *info,
+        iw_handler handler)
+{
+    printk("[%s %d] cmd 0x%x, info->cmd 0x%x\n", __func__, __LINE__, cmd, info->cmd); 
+
+	/* Always end with a call to jprobe_return(). */
+    jprobe_return();
+    return 0;
+}
+
+static int wext_handle_ioctl_entry(struct net *net, struct ifreq *ifr, unsigned int cmd, void __user *arg)
+{
+    printk("[%s %d] cmd 0x%x\n", __func__, __LINE__, cmd); 
+
+	/* Always end with a call to jprobe_return(). */
+    jprobe_return();
+    return 0;
+}
+
+/* 函数原型定义为static类型，所以register 会失败 */
+static int get_priv_descr_and_size_entry(struct net_device *dev, unsigned int cmd, const struct iw_priv_args **descrp)
+{
+    printk("[%s %d] cmd 0x%x\n", __func__, __LINE__, cmd); 
+    jprobe_return();
+    return 0;
+}
 
 /* For each probe you need to allocate a kprobe structure */
 static struct kprobe kp = {
-	//.symbol_name	= "do_fork", /* unknown symbol in module , why can not find it ? */
 	.symbol_name	= "loopback_xmit",
+};
+
+static struct jprobe debug_jprobe[] = { 
+    /* JPROBE_DEFINE(do_fork), */
+    JPROBE_DEFINE(wext_handle_ioctl),
+    JPROBE_DEFINE(ioctl_private_call),
+    /* JPROBE_DEFINE(get_priv_descr_and_size), will fail */
 };
 
 /* kprobe pre_handler: called just before the probed instruction is executed */
 static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 {
-#ifdef CONFIG_X86
 	printk(KERN_INFO "X86 pre_handler: p->addr = 0x%p, ip = %lx,"
 			" flags = 0x%lx\n",
 		p->addr, regs->ip, regs->flags);
-#endif
-#ifdef CONFIG_PPC
-	printk(KERN_INFO "PPC pre_handler: p->addr = 0x%p, nip = 0x%lx,"
-			" msr = 0x%lx\n",
-		p->addr, regs->nip, regs->msr);
-#endif
-#ifdef CONFIG_MIPS
-	printk(KERN_INFO "MIPS pre_handler: p->addr = 0x%p, epc = 0x%lx,"
-			" status = 0x%lx\n",
-		p->addr, regs->cp0_epc, regs->cp0_status);
-#endif
-#ifdef CONFIG_TILEGX
-	printk(KERN_INFO "TILEGX pre_handler: p->addr = 0x%p, pc = 0x%lx,"
-			" ex1 = 0x%lx\n",
-		p->addr, regs->pc, regs->ex1);
-#endif
 
 	/* A dump_stack() here will give a stack backtrace */
 	return 0;
@@ -52,22 +91,10 @@ static int handler_pre(struct kprobe *p, struct pt_regs *regs)
 static void handler_post(struct kprobe *p, struct pt_regs *regs,
 				unsigned long flags)
 {
-#ifdef CONFIG_X86
 	printk(KERN_INFO "post_handler: p->addr = 0x%p, flags = 0x%lx\n",
 		p->addr, regs->flags);
-#endif
-#ifdef CONFIG_PPC
-	printk(KERN_INFO "post_handler: p->addr = 0x%p, msr = 0x%lx\n",
-		p->addr, regs->msr);
-#endif
-#ifdef CONFIG_MIPS
-	printk(KERN_INFO "post_handler: p->addr = 0x%p, status = 0x%lx\n",
-		p->addr, regs->cp0_status);
-#endif
-#ifdef CONFIG_TILEGX
-	printk(KERN_INFO "post_handler: p->addr = 0x%p, ex1 = 0x%lx\n",
-		p->addr, regs->ex1);
-#endif
+
+    return;
 }
 
 /*
@@ -83,8 +110,42 @@ static int handler_fault(struct kprobe *p, struct pt_regs *regs, int trapnr)
 	return 0;
 }
 
+static int __init jprobe_init(void)
+{
+    int i = 0, j = 0;
+    int ret = 0;
+
+    for (i = 0; i < ARRAY_SIZE(debug_jprobe); i++) {
+        ret = register_jprobe(&debug_jprobe[i]);
+        if (ret < 0) {
+            printk("register_jprobe: func %s failed, returned %d\n", debug_jprobe[i].kp.symbol_name, ret);
+            goto bad;
+        }
+    }
+
+    return 0;
+
+bad:
+    for (j = 0; j < i; j++)
+        unregister_jprobe(&debug_jprobe[j]);
+
+    return -1;
+}
+
+static void __exit jprobe_exit(void)
+{
+    int i = 0;
+
+    for (i = 0; i < ARRAY_SIZE(debug_jprobe); i++)
+        unregister_jprobe(&debug_jprobe[i]);
+
+    return;
+}
+
+
 static int __init kprobe_init(void)
 {
+#if 0
 	int ret;
 	kp.pre_handler = handler_pre;
 	kp.post_handler = handler_post;
@@ -96,13 +157,19 @@ static int __init kprobe_init(void)
 		return ret;
 	}
 	printk(KERN_INFO "Planted kprobe at %p\n", kp.addr);
+#endif
+
+	printk(KERN_INFO "Planted kprobe\n");
+    jprobe_init();
+    
 	return 0;
 }
 
 static void __exit kprobe_exit(void)
 {
-	unregister_kprobe(&kp);
-	printk(KERN_INFO "kprobe at %p unregistered\n", kp.addr);
+	/* unregister_kprobe(&kp); */
+    jprobe_exit();
+	printk(KERN_INFO "kprobe unregistered\n");
 }
 
 module_init(kprobe_init)
